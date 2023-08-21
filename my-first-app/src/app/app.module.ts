f4106465b3ab33c6c960ac1fc9cff8fe47969713
grey <greyjing.zhou@ncsi.com.cn>
2023-08-21 16:40:50 +0800

    day01

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; // Import ServerComponent
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent // Add ServerComponent to declarations
   ],
   imports: [
     BrowserModule
