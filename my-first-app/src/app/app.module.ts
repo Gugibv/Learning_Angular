3688ed98201414f7f82dd56ba57f013ec0532b98
grey <greyjing.zhou@ncsi.com.cn>
2023-08-28 13:49:05 +0800

    update

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'; // Import ServerComponent
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component'; // Import ServerComponent
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent // Add ServerComponent to declarations
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent // Add ServerComponent to declarations
   ],
   imports: [
     BrowserModule
