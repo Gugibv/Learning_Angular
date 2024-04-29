95520e0a7180a294a2e29e306b82b3f731203ada
grey <greyjing.zhou@ncsi.com.cn>
2024-04-29 11:11:21 +0800

    add comment for extends SpringBootServletInitializer

--- my_first_app/angular_server/src/main/java/com/grey/BasicApplication.java
+++ my_first_app/angular_server/src/main/java/com/grey/BasicApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+/**
+ *   extends SpringBootServletInitializer
+ *
+ *   if not extends this class will get 404 error
+ */
 @SpringBootApplication
 public class BasicApplication  extends SpringBootServletInitializer {
 
