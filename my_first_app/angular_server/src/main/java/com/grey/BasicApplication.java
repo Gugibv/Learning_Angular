b971932405d422d8e0e5ae9866c1fec8b8cc57fa
grey <greyjing.zhou@ncsi.com.cn>
2024-04-29 11:08:34 +0800

    fix server cannot deploy issue

--- my_first_app/angular_server/src/main/java/com/grey/BasicApplication.java
+++ my_first_app/angular_server/src/main/java/com/grey/BasicApplication.java
@@ -2,9 +2,10 @@ package com.grey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BasicApplication {
+public class BasicApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicApplication.class,args);
