4da9164852df62bbe2928dc57b9cbf023f66c062
gugibv <gugibv@163.com>
2024-05-05 19:14:54 +0800

    add angular call api demo

---
+++ my_first_app/angular_client/src/app/data-display/data-display.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component,OnInit,inject } from '@angular/core';
+
+@Component({
+  selector: 'app-data-display',
+  standalone: true,
+  imports: [CommonModule,HttpClientModule],
+  templateUrl: './data-display.component.html',
+  styleUrl: './data-display.component.css'
+})
+export class DataDisplayComponent implements OnInit{
+
+  httpClient = inject(HttpClient);
+  data : any= [];
+  
+  
+   ngOnInit(): void {
+    this.httpClient.get('http://jsonplaceholder.typicode.com/posts').subscribe((data: any) => {
+      console.log(data);
+      this.data = data;
+    });
+   }
+
+}
--- my_first_app/angular_server/src/main/java/com/grey/controller/MobileController.java
+++ my_first_app/angular_server/src/main/java/com/grey/controller/MobileController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/mobile")
 public class MobileController {
@@ -11,5 +14,16 @@ public class MobileController {
     @GetMapping("/query")
     public String query(){
         return "mobile";
+    }
+
+    @GetMapping("/queryList")
+    public List<String> queryList(){
+        List<String> result = new ArrayList<>();
+        for(int i=10000;i<10010;i++){
+
+            result.add("phone num: "+ i);
+        }
+
+        return result;
     }
 }
